4a93d055a61a6c80a62fb94419061cac8c204a54
Augusto Saldanha <augusto.saldanha01@gmail.com>
2021-05-29 21:20:57 -0300

    Add lower in commands

--- sara.py
+++ sara.py
@@ -5,51 +5,50 @@ import threading
 import json
 
 from functions import *
+from pyYify import yify
 
-#create sara class
+
 class sara():
     def __init__(self, bot):
         self.bot = bot
-    #get message and chat id
     def handle(self, msg):
         chat_id = msg['chat']['id']
         command = msg['text']
-        #debug
+        
         print ('Received:')
         print(chat_id)
         print(command)
-        #compare chat id, only registered chats can control the bot!
-        if  chat_id == 1234567890:#need chat id
-            #now compare message to do something *Commands in portuguese, but you can change this!
+
+        if  chat_id == 1193864225:
             if command == '/oi':
                 self.bot.sendMessage(chat_id, str("Ola! Parabens, tudo funcionando"))
-            elif command == '/horas':
+            elif command.lower() == '/horas':
                 self.bot.sendMessage(chat_id, str("Horas: ") + str(now.hour) + str(":") + str(now.minute))
-            elif command == 'Desligue a luz do quarto':
-                self.bot.sendMessage(chat_id, str("Desligando..."))#sends the massage to bot.
-                os.system("mosquitto_pub -t esp32/light -m 0")# send the message to ESP32 through MQTT
-            elif command == 'Ligue a luz do quarto':
-                self.bot.sendMessage(chat_id, str("Ligando..."))
-                os.system("mosquitto_pub -t esp32/light -m 1")
-            elif command == 'Desligue a tv':
+            elif command.lower() == 'desligue a luz do quarto':
                 self.bot.sendMessage(chat_id, str("Desligando..."))
-                os.system("echo 'standby 0' | cec-client -s -d 1")# send command to HDMI through the CEC protocol
-            elif command == 'Ligue a tv':
+                os.system("mosquitto_pub -t Test -m 0")
+            elif command.lower() == 'ligue a luz do quarto':
+                self.bot.sendMessage(chat_id, str("Ligando..."))
+                os.system("mosquitto_pub -t Test -m 1")
+            elif command.lower() == 'desligue a tv':
+                self.bot.sendMessage(chat_id, str("Desligando..."))
+                os.system("echo 'standby 0' | cec-client -s -d 1")
+            elif command.lower() == 'ligue a tv':
                 self.bot.sendMessage(chat_id, str("Ligando..."))
                 os.system("echo 'on 0' | cec-client -s -d 1")
-            elif command == 'Aumente o volume':
+            elif command.lower() == 'aumente o volume':
                 self.bot.sendMessage(chat_id, str("Volume +1"))
                 os.system("echo 'volup 0' | cec-client -s -d 1")
-            elif command == 'Diminua o volume':
+            elif command.lower() == 'diminua o volume':
                 self.bot.sendMessage(chat_id, str("Volume -1"))
                 os.system("echo 'voldown 0' | cec-client -s -d 1")
-            elif command == 'Ligue o ps4':
+            elif command.lower() == 'ligue o ps4':
                 self.bot.sendMessage(chat_id, str("Ligando"))
                 os.system("echo 'on 4' | cec-client -s -d 1")
-            elif command == 'Modo de espera ps4':
+            elif command.lower() == 'modo de espera ps4':
                 self.bot.sendMessage(chat_id, str("Ligando"))
                 os.system("echo 'standby 4' | cec-client -s -d 1")
-            elif command == 'Temperatura':
+            elif command.lower() == 'temperatura':
                 self.bot.sendMessage(chat_id, str("Medindo"))
                 temp = subprocess.getoutput("mosquitto_sub -t esp32/temperature -R -C 1 -W 10")
                 if temp == "":
@@ -60,15 +59,15 @@ class sara():
                     self.bot.sendMessage(chat_id, str("Humidade: "))
                     humi = subprocess.getoutput("mosquitto_sub -t esp32/humidity -R -C 1")
                     self.bot.sendMessage(chat_id, str(humi + "%"))
-            elif command == '/help':
-                data = open('help.json', 'r')
-                hp = json.load(data)#load help data and sends to bot
+            elif command.lower() == '/help':
+                dados = open('help.json', 'r')
+                hp = json.load(dados)
                 for n in hp:
                     self.bot.sendMessage(chat_id, n)
                 dados.close()
                 del hp
             elif 'Youtube,' in command:
-                t = threading.Thread(target=getVideo, args=(chat_id, command, self.bot))#call function with another thread
+                t = threading.Thread(target=getVideo, args=(chat_id, command, self.bot))
                 t.start()
             elif 'Kodi' in command:
                 t = threading.Thread(target=openKodi, args=(chat_id, "kodi", self.bot))
@@ -80,12 +79,15 @@ class sara():
                     self.bot.sendMessage(chat_id, str("Bom filme :)"))
                 except:
                     self.bot.sendMessage(chat_id, str("Nao foi possivel executar"))
-            elif 'Serie,' or 'Filme,' or 'Anime,' or 'Musica,' in command:
+            elif 'serie,' in command.lower() or 'filme,' in command.lower() or 'anime,' in command.lower() or 'musica,' in command.lower():
                 try:
                     t = threading.Thread(target=searchTorrent, args=(chat_id, command, self.bot))
                     t.start()
                 except:
                     self.bot.sendMessage(chat_id, str("Houve um erro :( "))
+            else:
+
+                self.bot.sendMessage(chat_id, str('Comando nao reconhecido'))
         else:
             self.bot.sendMessage(chat_id, str("Celular nao cadastrado"))
 
